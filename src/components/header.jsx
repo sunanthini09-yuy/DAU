db1c825d3a43652f24f998246a34fb94a0ce74c4
Reverse01-Nake <reverse.deverloper@gmail.com>
2023-11-16 21:59:46 +0700

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
         bg-slate-400/80 border-slate-100/75 h-14 xl:px-64 lg:px-32 md:px-8 min-[920px]:px-16 backdrop-blur-sm'>
             <div className="flex items-center h-full">
                 <div id='logo' className="flex items-center h-full hover:scale-110 ease-in-out duration-300">
-                    <img className=' h-32 w-auto mr-1' src={logo} alt="" />
+                    <img className=' h-32 w-auto mr-2' src={logo} alt="" />
                 </div>
             </div>  
         </nav>
